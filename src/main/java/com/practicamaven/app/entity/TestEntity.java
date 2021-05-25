e7cd26edb9e8209afac49cd8bac3e30d4a33fbb7
MarcosHdz18 <marcoshernandezg18@yahoo.com>
2021-05-24 20:13:24 -0500

    Nombre del proyecto: my-app, Descripcion: Version final del proyecto my-app

---
+++ src/main/java/com/practicamaven/app/entity/TestEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.practicamaven.app.entity;
+
+/**
+ * @author marcos.hernandez
+ *
+ */
+public class TestEntity {
+
+	/**
+	 * Propiedad de prueba para la muestra de compilacion del archivo TestEntity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
